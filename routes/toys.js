cc4808b182df200da2f4a102d7ee31ffeeb2168a
saralb03 <saralea.brodsky@gmail.com>
2023-12-05 11:05:23 +0200

    clean code

--- routes/toys.js
+++ routes/toys.js
@@ -1,157 +1,152 @@
 const express = require("express");
-const {auth} = require("../middlewares/auth");
-const { ToyModel, validateToy } = require("../models/toyModel");
+const { auth } = require("../middlewares/auth");
+const { ToyModel, validateToy } = require("../models/toyModel")
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  let perPage = req.query.perPage || 10;
-  let page = req.query.page || 1;
-  try {
-    let data = await ToyModel.find({})
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "there error try again later", err });
-  }
-});
+    let perPage = req.query.perPage || 10;
+    let page = req.query.page || 1;
+    try {
+        let data = await ToyModel.find({})
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({ _id: -1 })
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error, try again later", err })
+    }
+})
 
 router.get("/search", async (req, res) => {
-  let perPage = req.query.perPage || 10;
-  let page = req.query.page || 1;
-  try {
-    let queryS = req.query.s;
-    let searchReg = new RegExp(queryS, "i")
-    let data = await ToysModel.find({ $or: [{ name: searchReg }, { info: searchReg }] })
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 })
-    res.json(data);
-  }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "there error try again later", err })
-  }
+    let perPage = req.query.perPage || 10;
+    let page = req.query.page || 1;
+    try {
+        let queryS = req.query.s;
+        let searchReg = new RegExp(queryS, "i")
+        let data = await ToyModel.find({ $or: [{ name: searchReg }, { info: searchReg }] })
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({ _id: -1 })
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to search, try again later", err })
+    }
+})
+
+router.get("/category/:catname", async (req, res) => {
+    let perPage = req.query.perPage || 10;
+    let page = req.query.page || 1;
+    try {
+        let category = req.params.catname;
+        let searchReg = new RegExp(category, "i")
+        let data = await ToyModel.find({ category: searchReg })
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({ _id: -1 })
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error, try again later", err })
+    }
+})
+
+router.post("/", auth, async (req, res) => {
+    let validBody = validateToy(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    try {
+        let toy = new ToyModel(req.body);
+        toy.user_id = req.tokenData._id;
+        await toy.save();
+        res.status(201).json(toy);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to add, try again later", err })
+    }
+})
+
+router.put("/:editId", auth, async (req, res) => {
+    let validBody = validateToy(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    try {
+        let editId = req.params.editId;
+        let data;
+        if (req.tokenData.role == "admin") {
+            data = await ToyModel.updateOne({ _id: editId }, req.body)
+        }
+        else {
+            data = await ToyModel.updateOne({ _id: editId, user_id: req.tokenData._id }, req.body)
+        }
+        if (data.modifiedCount == 0) {
+            res.json({ msg: "not valid id or you are not allowed to edit. nothing was edited" })
+        }
+        else res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to edit, try again later", err })
+    }
+})
+
+router.delete("/:delId", auth, async (req, res) => {
+    try {
+        let delId = req.params.delId;
+        let data;
+        if (req.tokenData.role == "admin") {
+            data = await ToyModel.deleteOne({ _id: delId })
+        }
+        else {
+            data = await ToyModel.deleteOne({ _id: delId, user_id: req.tokenData._id })
+        }
+        if (data.deletedCount == 0) {
+            res.json({ msg: "not valid id or you are not allowed to erase. nothing was erased" })
+        }
+        else res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to delete, try again later", err })
+    }
 })
 
 router.get("/prices", async (req, res) => {
-  let perPage = req.query.perPage || 10;
-  let page = req.query.page || 1;
-  try {
-    let minPrice = req.query.min;
-    let maxPrice = req.query.max;
-    if(minPrice&maxPrice){
-      let data = await ToyModel.find({$and:[{price:{$gte:minPrice}},{price:{$lte:maxPrice}}]})
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-       res.json(data);
+    let perPage = req.query.perPage || 10;
+    let page = req.query.page || 1;
+    try {
+        let min = req.query.min || 0;
+        let max = req.query.max || Infinity;
+        let data = await ToyModel.find({ $and: [{ price: { $gte: min } }, { price: { $lte: max } }] })
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({ _id: -1 })
+        res.json(data)
     }
-    else if(minPrice){
-      let data = await ToyModel.find({price:{$gte:minPrice}})
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-       res.json(data);
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error, try again later", err })
     }
-    else if(maxPrice){
-      let data = await ToyModel.find({price:{$lte:maxPrice}})
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-       res.json(data);
-    }
-    else{
-      let data = await ToyModel.find({})
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-       res.json(data);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "err", err });
-  }
-});
-
-router.get("/category/:catname", async (req, res) => {
-  let perPage = req.query.perPage || 10;
-  let page = req.query.page || 1;
-  try {
-    let category = req.params.catname;
-    let searchReg = new RegExp(category, "i")
-    let data = await ToysModel.find({ category: searchReg })
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 })
-    res.json(data);
-  }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "there error try again later", err })
-  }
-
 })
 
 router.get("/single/:id", async (req, res) => {
-  let perPage = req.query.perPage || 10;
-  let page = req.query.page || 1;
-  try {
-    let idT = req.params.id;
-    let data = await ToyModel.find({ _id: idT })
-      .limit(perPage)
-      .skip((page - 1) * perPage)
-      .sort({ _id: -1 });
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "err", err });
-  }
-});
-
-router.post("/", auth, async(req,res) => {
-  let valdiateBody = validateToy(req.body);
-  if (valdiateBody.error) {
-    return res.status(400).json(valdiateBody.error.details);
-  }
-  try {
-    let toy = new ToyModel(req.body);
-      toy.user_id = req.tokenData._id;
-    await toy.save();
-    res.status(201).json(toy);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "err", err });
-  }
-});
-router.put("/:idEdit",auth, async(req,res) => {
-  let valdiateBody = validateToy(req.body);
-  if (valdiateBody.error) {
-    return res.status(400).json(valdiateBody.error.details);
-  }
-  try {
-    let idEdit = req.params.idEdit;
-    let data = await ToyModel.updateOne({ _id: idEdit }, req.body);
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "err", err });
-  }
-});
-router.delete("/:idDel",auth, async(req,res) => {
-  try {
-    let idDel = req.params.idDel;
-    let data = await ToyModel.deleteOne({ _id: idDel });
-    if (data.deletedCount == 0) {
-      return res.status(403).json({ msg: "Unauthorized to delete this toy" });
+    try {
+        let id = req.params.id;
+        let data = await ToyModel.findById(id)
+        res.json(data);
     }
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "err", err });
-  }
-});
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error, try again later", err })
+    }
+})
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,81 +1,140 @@
-const express= require("express");
+const express = require("express");
 const bcrypt = require("bcrypt");
-const {auth, authAdmin} = require("../middlewares/auth");
-const {UserModel,validUser, validLogin,createToken} = require("../models/userModel")
+const { UserModel, validateUser, loginValid, createToken } = require("../models/userModel")
+const { auth, authAdmin } = require("../middlewares/auth")
 const router = express.Router();
 
-router.get("/" , async(req,res)=> {
-  res.json({msg:"Users work"})
-})
+router.get("/", async (req, res) => {
+    let perPage = Math.min(req.query.perPage, 20) || 10;
+    let page = req.query.page || 1;
+    let sort = req.query.sort || "_id";
+    let reverse = req.query.reverse == "yes" ? -1 : 1;
 
-router.get("/myInfo",auth, async(req,res) => {
-  try{
-    let userInfo = await UserModel.findOne({_id:req.tokenData._id},{password:0});
-    res.json(userInfo);
-  }
-  catch(err){
-    console.log(err)
-    res.status(500).json({msg:"err",err})
-  }  
-})
-
-router.get("/usersList", authAdmin , async(req,res) => {
-  try{
-    let data = await UserModel.find({},{password:0});
-    res.json(data)
-  }
-  catch(err){
-    console.log(err)
-    res.status(500).json({msg:"err",err})
-  }  
-})
-
-
-
-router.post("/", async(req,res) => {
-  let validBody = validUser(req.body);
-  if(validBody.error){
-    return res.status(400).json(validBody.error.details);
-  }
-  try{
-    let user = new UserModel(req.body);
-    user.password = await bcrypt.hash(user.password, 10);
-    await user.save();
-    user.password = "*******";
-    res.status(201).json(user);
-  }
-  catch(err){
-    if(err.code == 11000){
-      return res.status(500).json({msg:"Email already in system, try log in",code:11000})
-       
+    try {
+        let data = await UserModel
+            .find({},{name:1})
+            .limit(perPage)
+            .skip((page - 1) * perPage)
+            .sort({ [sort]: reverse })
+        res.json(data);
     }
-    console.log(err);
-    res.status(500).json({msg:"err",err})
-  }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
 })
 
-
-router.post("/login", async(req,res) => {
-  let validBody = validLogin(req.body);
-  if(validBody.error){
-    return res.status(400).json(validBody.error.details);
-  }
-  try{
-    let user = await UserModel.findOne({email:req.body.email})
-    if(!user){
-      return res.status(401).json({msg:"Password or email is worng ,code:1"})
+router.get("/myInfo", auth, async (req, res) => {
+    try {
+        let userInfo = await UserModel.findOne({ _id: req.tokenData._id }, { password: 0 });
+        res.json(userInfo);
     }
-    let authPassword = await bcrypt.compare(req.body.password,user.password);
-    if(!authPassword){
-      return res.status(401).json({msg:"Password or email is worng ,code:2"});
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "err", err });
     }
-    let token = createToken(user._id,user.role);
-    res.json({token});
-  }
-  catch(err){
-    console.log(err)
-    res.status(500).json({msg:"err",err})
-  }
 })
+
+router.get("/usersList", authAdmin, async (req, res) => {
+    try {
+        let data = await UserModel.find({}, { password: 0 });
+        res.json(data)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
+})
+
+router.post("/", async (req, res) => {
+    let valdiateBody = validateUser(req.body);
+    if (valdiateBody.error) {
+        return res.status(400).json(valdiateBody.error.details)
+    }
+    try {
+        let user = new UserModel(req.body);
+        user.password = await bcrypt.hash(user.password, 10)
+        await user.save();
+        user.password = "******";
+        res.status(201).json(user)
+    }
+    catch (err) {
+        if (err.code == 11000) {
+            return res.status(400).json({ msg: "Email already in system try login", code: 11000 })
+        }
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
+})
+
+router.post("/login", async (req, res) => {
+    let valdiateBody = loginValid(req.body);
+    if (valdiateBody.error) {
+        return res.status(400).json(valdiateBody.error.details)
+    }
+    try {
+        let user = await UserModel.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(401).json({ msg: "Email or password is worng, code:1" })
+        }
+        let validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ msg: "Email or password is worng, code:2" })
+        }
+        let token = createToken(user._id, user.role);
+        res.json({ token: token });
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: "err", err })
+    }
+})
+
+router.put("/:editId", auth, async (req, res) => {
+    let validBody = validateUser(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    try {
+        let editId = req.params.editId;
+        let data;
+        req.body.password = await bcrypt.hash(req.body.password, 10);
+        console.log(req.body);
+        if (req.tokenData.role == "admin") {
+            data = await UserModel.updateOne({ _id: editId }, req.body)
+        }
+        else if (editId == req.tokenData._id) {
+            data = await UserModel.updateOne({ _id: editId }, req.body);
+        }
+        else {
+            data = [{ status: "failed", msg: "You are not allowed to edit" }]
+        }
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to edit user, try again later", err })
+    }
+})
+
+router.delete("/:delId", auth, async (req, res) => {
+    try {
+        let delId = req.params.delId;
+        let data;
+        if (req.tokenData.role == "admin") {
+            data = await UserModel.deleteOne({ _id: delId })
+        }
+        else {
+            data = [{ status: "failed", msg: "You are not admin" }]
+        }
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "There error to delete, try again later", err })
+    }
+});
+
+
 
 module.exports = router;
